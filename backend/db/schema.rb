b293c9a541857d03bd88eaf78c3843d35711b5fb
Rifki Fauzi <kubido@gmail.com>
2017-03-13 01:28:19 +0700

    remove answer model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301141724) do
+ActiveRecord::Schema.define(version: 20170301141715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "answers", force: :cascade do |t|
-    t.integer  "question_id"
-    t.string   "content"
-    t.boolean  "correct"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
 
   create_table "questions", force: :cascade do |t|
     t.text     "content"

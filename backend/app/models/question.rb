c20d0aa25e8a817be99e9c7286208676556e796b
Rifki Fauzi <kubido@gmail.com>
2017-03-13 07:05:30 +0700

    add test case for create, update and delete

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -14,6 +14,12 @@ require 'numbers_in_words'
 class Question < ApplicationRecord
   validates_presence_of :content, :answer
 
+  # before_create :convert_answer_number_to_words
+
+  # def convert_answer_number_to_words
+  #   answer = NumbersInWords.in_words(answer) if answer.is_a?(Numeric)
+  # end
+
   def attributes
     {
       id: id,
--- backend/spec/models/question_spec.rb
+++ backend/spec/models/question_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 
 RSpec.describe Question, :type => :model do
-  it "is valid create question with valid attributes"
-  it "is valid update question"
-  it "is valid delete question"
+  subject { described_class.create(content: "is USA is a country ?", answer: "Yes") }
+
+
+  it "is valid create question with valid attributes" do 
+    expect(subject).to be_valid
+  end
+
+  it "is valid update question" do 
+    success = subject.update_attributes(content: "is Earth is a country ?", answer: "No" )
+    expect(success).to eq(true)
+  end
+
+  it "is valid delete question" do 
+    question = Question.create(content: "is USA is a country ?", answer: "Yes")
+    question.destroy
+    counter = Question.all.count
+    expect(counter).to eq(0)
+  end
+
   it "is valid answer with words"
   it "is valid answer with integer"
 end
